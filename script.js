467ce732b1a53f4520bcba9f58208b16a19f8c22
tsukasadh <110469570+tsukasadh@users.noreply.github.com>
2022-08-25 18:15:01 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+const target = document.getElementById("target");
+let array = ["html", "css", "(php)", "javascript"];
+let wordIndex = 0;
+let letterIndex = 0;
+
+const createLetter = () => {
+  const letter = document.createElement("span");
+  target.appendChild(letter);
+
+  letter.classList.add("letter");
+  letter.style.opacity = '0';
+  letter.style.animation = "anim 5s ease forwards";
+  letter.textContent = array[wordIndex][letterIndex];
+
+  setTimeout(() => {
+    letter.remove();
+  }, 2000);
+};
+
+const loop = () => {
+  setTimeout(() => {
+    if (wordIndex >= array.length) {
+      wordIndex = 0;
+      letterIndex = 0;
+      loop();
+      
+    } else if (letterIndex < array[wordIndex].length) {
+      createLetter();
+      letterIndex++;
+      loop();
+      
+    } else {
+      letterIndex = 0;
+      wordIndex++;
+      setTimeout(() => {
+        loop();
+      }, 2000);
+    }
+  }, 80);
+}
+loop();
+
+/* Shapes */
+var svgContainer = document.getElementById('svgContainer');
+var animItem = bodymovin.loadAnimation({
+  wrapper: svgContainer,
+  animType: 'svg',
+  loop: true,
+  path: 'https://labs.nearpod.com/bodymovin/demo/markus/isometric/markus2.json'
+});
+
